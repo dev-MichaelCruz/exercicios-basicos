87350def95753387dfce99392cfec73b7836a073
dev-MichaelCruz <michaelcruz.dev@gmail.com>
2023-09-29 14:20:36 -0300

    Tabuada adicionada

---
+++ Tabuada.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("==============================================================");
+        System.out.println("Este programa te apresenta uma tabuada de um número qualquer");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Digite um número: ");
+        int numero = leitor.nextInt();
+
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Tabuada do " + numero);
+        System.out.println("-------------------------------------------------------------");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+        System.out.println("==============================================================");
+
+    }
+}
