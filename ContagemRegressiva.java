7abaaf63ade1074ce2b1cb07325c0e414394fad6
Michael Cruz <michael.cruz@ibm.com>
2023-09-29 13:09:01 -0300

    Contagem Regressiva adicionada

---
+++ ContagemRegressiva.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ContagemRegressiva {
+  public static void main(String[] args) {
+
+    Scanner leitor = new Scanner(System.in);
+    System.out.println("=================================================");
+    System.out.println("Este programa executa uma contagem regressiva!");
+    System.out.println("Digite um número para executar");
+    int numero = leitor.nextInt();
+    System.out.println("---------------------------------------------------");
+    while (numero >= 1) {
+      System.out.println(numero);
+      numero--;
+    }
+    System.out.println("=================================================");
+  }
+}
