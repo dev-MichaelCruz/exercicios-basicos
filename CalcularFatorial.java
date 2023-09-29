acfbfd933f724f2e45f5eddbcfb2c7c208bd8078
Michael Cruz <michael.cruz@ibm.com>
2023-09-29 12:57:00 -0300

    Calcular Fatorial

--- CalcularFatorial.java
+++ CalcularFatorial.java
@@ -5,10 +5,21 @@ public class CalcularFatorial {
   public static void main(String[] args) {
 
     Scanner leitor = new Scanner(System.in);
-
+    System.out.println("=================================================");
     System.out.println("Programa para calcular o fatorial de um número!");
-    System.out.println("Insira um número para calsular o Fatorial");
+    System.out.println("Insira um número para calcular o Fatorial");
+    System.out.println("--------------------------------------------------");
+    int numero = leitor.nextInt();
 
+    long fatorial = 1;
+
+    for (int i = 1; i <= numero; i++) {
+      fatorial = fatorial * i;
+    }
+
+    System.out.println("--------------------------------------------------");
+    System.out.println("O fatorial de " + numero + " é igual a " + fatorial);
+    System.out.println("==================================================");
   }
 
 }
