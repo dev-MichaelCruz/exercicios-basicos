9b3b2fe98593b3190400e345464dac972ed6d37d
Michael Cruz <michael.cruz@ibm.com>
2023-09-29 12:41:23 -0300

    Calculadora simples

--- Calculadora.java
+++ Calculadora.java
@@ -6,22 +6,25 @@ public class Calculadora {
 
     Scanner leitor = new Scanner(System.in);
     float resultado = 0;
-
+    System.out.println("==========================================================");
     System.out.println("Esta é uma calculadora para realizar operações simples!");
     System.out.println("Que tipo de operação deseja realizar?");
-
+    System.out.println("-------------------------------------------------------------");
     System.out.println("1 - Adição");
     System.out.println("2 - Subtração");
     System.out.println("3 - Multiplicação");
     System.out.println("4 - divisão");
+    System.out.println("---------------------------------------------------------------");
     int operacao = leitor.nextInt();
-
+    System.out.println("---------------------------------------------------------------");
     System.out.println("Ok, você escolheu a opção " + operacao);
     System.out.println("Digite agora o primeiro numero:");
     float primeiroNumero = leitor.nextFloat();
 
+    System.out.println("---------------------------------------------------------------");
     System.out.println("Digite agora o segundo número:");
     float segundoNumero = leitor.nextFloat();
+    System.out.println("---------------------------------------------------------------");
 
     switch (operacao) {
       case 1 -> resultado = primeiroNumero + segundoNumero;
@@ -31,6 +34,7 @@ public class Calculadora {
     }
 
     System.out.println("O resultado da operação é: " + resultado);
+    System.out.println("==========================================================");
 
     main(args);
   }
---
+++ CalcularFatorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CalcularFatorial {
+
+  public static void main(String[] args) {
+
+    Scanner leitor = new Scanner(System.in);
+
+    System.out.println("Programa para calcular o fatorial de um número!");
+    System.out.println("Insira um número para calsular o Fatorial");
+
+  }
+
+}
